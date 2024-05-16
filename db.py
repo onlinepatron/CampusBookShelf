78b72cf61c7b31c0a9686dc14056c38f31b28b4a
Camilo <23364195@student.uwa.edu.au>
2024-05-16 20:49:09 +0800

    implemented get_user, add_user, and remove_user in db.py.

--- db.py
+++ db.py
@@ -1,4 +1,25 @@
 from app import db
+from models import User
+
+def get_user(username):
+    return User.query.filter_by(username=username).first()
+
+def add_user(username, password):
+    if get_user(username):
+        return False
+    new_user = User(username=username)
+    new_user.set_password(password)
+    db.session.add(new_user)
+    db.session.commit()
+    return True
+
+def remove_user(username):
+    user = get_user(username)
+    if user:
+        db.session.delete(user)
+        db.session.commit()
+        return True
+    return False
 
 def get_db():
     return db
